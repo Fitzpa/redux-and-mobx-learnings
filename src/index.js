99ee90fbe6a1d1daa24be6db5ef0cf11b1b85052
unknown <fitz4492@gmail.com>
2020-05-12 21:41:03 -0500

    created redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { connnect, Provider } from 'react-redux'
+
 import './styles.scss';
+
+const initialState = {
+    count = 0
+}
+
+// Putting a string that will be used throughout your application into a variable like this allows it to error if is misspelled down the road.
+// This is a good way to prevent that kind of bug later on.
+const INCREMENT = 'INCREMENT'
+
+// ACTION
+const incrementValue = () => ({
+    type: INCREMENT
+})
+
+// REDUCER
+const reducer = (state = initialState, action) => {
+    if (action.type === INCREMENT) {
+        return {
+            count: state.count + 1
+        }
+    }
+
+    return state
+}
+
+const store = createStore(reducer)
 
 class Counter extends Component {
     render() {
